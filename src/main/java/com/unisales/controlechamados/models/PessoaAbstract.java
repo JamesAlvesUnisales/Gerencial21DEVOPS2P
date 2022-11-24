a6444223cb7b1960971aced867de71d03978d08a
James Alves <james@jamesalves.com.br>
2022-11-24 20:17:13 -0300

    Criado classe abstrata e feito o extends

---
+++ src/main/java/com/unisales/controlechamados/models/PessoaAbstract.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.unisales.controlechamados.models;
+
+/**
+ *
+ * @author James
+ */
+public abstract class PessoaAbstract {
+    private String nome;
+    private String cargo;
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
--- src/main/java/com/unisales/controlechamados/models/Tecnico.java
+++ src/main/java/com/unisales/controlechamados/models/Tecnico.java
@@ -8,35 +8,18 @@ package com.unisales.controlechamados.models;
  *
  * @author James
  */
-public class Tecnico {
-    private String nome;
-    private String cargo;
+public class Tecnico extends PessoaAbstract{
+
     private String nivel;
 
     public Tecnico(String nome, String cargo, String nivel) {
-        this.nome = nome;
-        this.cargo = cargo;
+        this.setNome(nome);
+        this.setCargo(cargo);
         this.nivel = nivel;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getCargo() {
-        return cargo;
     }
 
     public String getNivel() {
         return nivel;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
     }
 
     public void setNivel(String nivel) {
@@ -46,7 +29,5 @@ public class Tecnico {
     @Override
     public String toString() {
         return super.toString(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
-    }
-    
-    
+    } 
 }
