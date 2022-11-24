1d4f666d2752b21e11074727b6ab344b03e47a27
James Alves <james@jamesalves.com.br>
2022-11-24 20:40:16 -0300

    refatore : cliente.java

---
+++ src/main/java/com/unisales/controlechamados/models/Cliente.java
@@ -0,0 +1,33 @@
+package com.unisales.controlechamados.models;
+
+  /* author Caio
+*/
+public class Cliente extends PessoaAbstract{
+    
+    private String email;
+    private String telefone;
+    
+    public Cliente(String nome, String cargo, String email, String telefone){
+        this.setNome(nome);
+        this.setCargo(cargo);
+
+        this.setEmail(email);
+        this.setTelefone(telefone);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
